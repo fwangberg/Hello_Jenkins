4a68ef035b9d06e9c02d7ee8e9ac4143113be00b
Frank Wangberg <fwangberg@mail.csuchico.edu>
2017-02-08 12:38:16 -0800

    Fixed the error!

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void)
 {
-  cout << "Hello, World\n"://<--that's not a semicolon
+  cout << "Hello, World\n"; //://<--that's not a semicolon
   cout << "Hello, Jenkins\n";
 
   cout << "I have successfully built and run\n";
